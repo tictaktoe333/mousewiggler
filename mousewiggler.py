0f8288b96492197f0428c5d4414040bbf464e36f
tictaktoe333 <tom.ski@hotmail.co.uk>
2023-12-10 20:39:02 +0000

    Initial commit

---
+++ mousewiggler.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+# Mouse wiggler
+
+import mouse
+import random
+
+from time import sleep
+
+
+def setup():
+    """Returns a tuple containing the mouse's starting XY position."""
+    return mouse.get_position()
+
+
+def run(start_position):
+    """Find current position of mouse and run main loop."""
+    current = start_position
+    print(f"{current}")
+
+    while True:
+        x_change = random.randrange(11, 110, 1)
+        y_change = random.randrange(11, 110, 1)
+        mouse.move(current[0]+x_change, current[1]+y_change, duration=1)
+        waiting = random.randrange(11, 54, 1)
+        sleep(waiting)
+
+
+if __name__ == "__main__":
+    start_position = setup()
+    run(start_position)
